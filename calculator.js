0a8034756d7236fb2144a0dc633598bc6caf3a03
lohikaarme <stewart.watt@gmail.com>
2022-05-11 23:39:45 -0700

    created calculator.js, added general operator functions

---
+++ calculator.js
@@ -0,0 +1,8 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let operate = (a, b, operator) => operator(a, b);
+
+console.log(operate(4,5,multiply));
