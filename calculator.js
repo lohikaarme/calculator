4c32a9a54baa147da10a1cf73f35d3e16ed3b7b7
lohikaarme <stewart.watt@gmail.com>
2022-05-15 21:55:19 -0700

    Added update and clearing memory functions

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ let storedValue = [];
 let storedOperator = [];
 let input = '';
 let total = 0;
-let displayValue = '';
+let displayValue = '0';
 const screen = document.querySelector('.screen');
 
 let add = (a, b) => a + b;
@@ -11,6 +11,8 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
 let operate = (a, b, operator) => operator(a, b);
+
+let updateDisplay = (displayValue, input) => displayValue = displayValue.concat(input)
 
 const keys = document.querySelectorAll('[data-key]');
 const keyPress = keys.forEach((key) => { // keyPress accepts a button press and assigns data-key attribute to input
@@ -25,16 +27,29 @@ const keyPress = keys.forEach((key) => { // keyPress accepts a button press and 
 const operators = document.querySelectorAll('[data-operator]');
 const operatorPress = operators.forEach((operator) => { 
 	operator.addEventListener('click', () => {
-		storedValue.push(+displayValue);
-		displayValue = '';
 		input = operator.getAttribute('data-operator');
-		storedOperator.push(input);
+		updateMemory(input);
 		screen.textContent = input;
 		console.log(input);
 	});
 });
 
-let updateDisplay = (displayValue, input) => displayValue = displayValue.concat(input)
+let updateMemory = (input) => {
+	storedOperator.push(input);
+	storedValue.push(+displayValue);
+	displayValue = '';
+};
+
+let clearMemory = () => {
+	storedValue = [];
+	storedOperator = [];
+	input = '';
+	total = 0;
+	displayValue = '0';
+	screen.textContent = displayValue;
+};
+
+
 
 console.log(typeof keys)
 console.log(keys)
