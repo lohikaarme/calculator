b72417909387c1e74bc998b1f55415c4c0bdb535
lohikaarme <stewart.watt@gmail.com>
2022-05-15 18:05:53 -0700

    Value updating based on user input and printing to calculator screen

--- calculator.js
+++ calculator.js
@@ -1,4 +1,10 @@
 
+let storedValueA = 0;
+let storedValueB = 0;
+let input = '';
+const screen = document.querySelector('.screen');
+let displayValue = '';
+let total = 0;
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
@@ -7,9 +13,16 @@ let divide = (a, b) => a / b;
 let operate = (a, b, operator) => operator(a, b);
 
 const keys = document.querySelectorAll('[data-key]');
-keys.forEach((key) => {
-	key.addEventListener('click', () => console.log(key.getAttribute('data-key')));
+const keyPress = keys.forEach((key) => { // keyPress accepts a button press and assigns data-key attribute to input
+	key.addEventListener('click', () => {
+		input = key.getAttribute('data-key');
+		displayValue = displayValue.concat(input);
+		screen.textContent = displayValue;
+		console.log(input);
+	});
 });
+
+let updateDisplay = (displayValue, input) => displayValue = displayValue.concat(input)
 
 console.log(typeof keys)
 console.log(keys)
