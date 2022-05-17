906163dea7269fafb51c88369d91d43820c20c91
lohikaarme <stewart.watt@gmail.com>
2022-05-16 22:55:25 -0700

    Typo correction in operate function

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ let storedValue = [];
 let storedOperator = [];
 let input = '';
 let total = 0;
-let displayValue = '0';
+let displayValue = ' ';
 const screen = document.querySelector('.screen');
 
 let add = (a, b) => a + b;
@@ -14,7 +14,7 @@ let operate = (a, b, operator) => {
 	if (operator == '+') return add(a, b);
 	if (operator == '-') return subtract(a, b);
 	if (operator == '*') return multiply(a, b);
-	if (operator == '%') return divided(a, b);
+	if (operator == '%') return divide(a, b);
 };
 
 const keys = document.querySelectorAll('[data-key]');
