86372315fe801cc5818ec9e104fa9709253591b7
lohikaarme <stewart.watt@gmail.com>
2022-05-17 23:05:42 -0700

    Simplified calculator and cleaned functions

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 
-let storedValue = [];
-let storedOperator = [];
+let storedValue = '';
+let storedOperator = '';
 let input = '';
 let total = 0;
 let displayValue = ' ';
 const screen = document.querySelector('.screen');
 
-let add = (a, b) => a + b;
+let add = (a, b) => +a + +b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
@@ -15,20 +15,13 @@ let operate = (a, b, operator) => {
 	switch (operator) {
 		case '+':
 			return add(a, b);
-			break;
 		case '-':
 			return subtract(a, b);
-			break;
 		case '*':
 			return multiply(a, b);
-			break;
 		case '%':
 			return divide(a, b);
-			break;
 		case '=':
-			break;
-		case 'blank':
-			return a;
 			break;
 		default:
 			break;
@@ -49,46 +42,24 @@ const operators = document.querySelectorAll('[data-operator]');
 const operatorPress = operators.forEach((operator) => { 
 	operator.addEventListener('click', () => {
 		input = operator.getAttribute('data-operator');
-		updateMemory(input);
-		screen.textContent = input;
-		console.log(input);
+		// updateMemory(input);
+		if (!storedOperator) {
+			total = displayValue;
+			storedOperator = input;
+			displayValue = ''
+		} else {
+			total = operate(total, displayValue, storedOperator);
+			storedOperator = input;
+			displayValue = ''
+			updateScreen(total)
+		};
 	});
 });
-
-let calculate = () => {
-	total = storedValue[0];
-	for (let i = 0; i < (storedOperator.length -1); i++) {
-		total = operate(total, storedValue[i + 1], storedOperator[i]);
-	let temp = total;
-	clearMemory();
-	displayValue = temp;
-	updateScreen();
-	updateMemory('blank');
-
-	}
-};
 
 let updateScreen = (input) => {
 	if (!input) input = displayValue;
 	screen.textContent = input;
 };
-
-let updateMemory = (input) => {
-	storedOperator.push(input);
-	storedValue.push(+displayValue);
-	displayValue = '';
-};
-
-let clearMemory = () => {
-	storedValue = [];
-	storedOperator = [];
-	input = '';
-	total = 0;
-	displayValue = '0';
-	screen.textContent = displayValue;
-};
-
-
 
 console.log(typeof keys)
 console.log(keys)
